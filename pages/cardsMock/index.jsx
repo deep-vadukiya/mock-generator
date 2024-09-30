b9e500a2e7d3de85c098b0cced76386630d6e791
Deep Vadukiya <dipvadukiya06@gmail.com>
2024-09-30 13:43:32 +0530

    MG02 - credit card - added google fonts and respective updates

--- pages/cardsMock/index.jsx
+++ pages/cardsMock/index.jsx
@@ -7,6 +7,8 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 //
 import { APP_PATH } from "../../routes/paths";
+import CardChip from "../../assets/creditCard/cardChip";
+import Nfc from "../../assets/creditCard/Nfc";
 
 // ------------------------------------------------
 
@@ -17,9 +19,14 @@ export default function CardMock() {
 
   const bankName = "HDFC Bank";
   const bankPartnerName = "TATA NEUCARD PLUS";
-  const cardType = "Rupay";
+  const cardCategory = "Rupay";
   const cardTypeCategory = "PLATINUM";
   const backgroundColor = "#8CC0DE";
+  const cardType = "CREDIT CARD";
+  const cardNumber = "6529 2500 0082 1348";
+  const cardHolderName = "DEEP VADUKIYA";
+  const cardExpires = "04/28";
+  const cardCVV = "04/28";
 
   return (
     <div>
@@ -30,34 +37,68 @@ export default function CardMock() {
       <div className="h-36" />
 
       <div
-        className="container mx-auto p-10 rounded-xl shadow-xl"
-        style={{ backgroundColor }}
+        className="container mx-auto p-12 rounded-3xl shadow-xl flex flex-col justify-between"
+        style={{ backgroundColor, maxWidth: 880, height: 510 }}
       >
-        <div style={{ maxWidth: 1011, maxHeight: 638 }}>
-          <div className="w-full h-full">
-            <div className="w-full flex justify-between">
-              <div className="space-y-2">
-                <p className="font-sans text-3xl font-bold text-white">
-                  {bankName}
-                </p>
-                <p className="font-sans text-2xl text-white opacity-70">
-                  {bankPartnerName}
-                </p>
-              </div>
+        <div className="w-full flex justify-between">
+          <div className="space-y-2">
+            <p className="text-4xl font-semibold text-white">{bankName}</p>
+            <p className="text-3xl text-white opacity-70">{bankPartnerName}</p>
+          </div>
 
-              <div className="space-y-2 justify-end">
-                <p className="font-sans text-3xl font-semibold text-white text-end">
-                  {cardType}
-                </p>
-                <p className="font-sans text-xl font-thin text-white">
-                  {cardTypeCategory}
-                </p>
+          <div className="space-y-2 justify-end">
+            <p className="text-3xl font-semibold text-white text-end">
+              {cardCategory}
+            </p>
+            <p className="text-xl font-thin text-white">{cardTypeCategory}</p>
+          </div>
+        </div>
+
+        <div className="w-full flex justify-between">
+          <CardChip height={45} width={56} />
+          <Nfc />
+        </div>
+
+        <div className="flex flex-row">
+          <div className="basis-8/12">
+            <div className="w-full flex flex-col justify-between">
+              <p className="text-white font-extralight text-5xl tracking-widest pb-6">
+                {cardNumber}
+              </p>
+
+              <div className="flex justify-between">
+                <div className="">
+                  <p className="text-white text-2xl font-extralight opacity-60 max-w-fit">
+                    Card Holder
+                  </p>
+                  <p className="text-white text-2xl max-w-fit">
+                    {cardHolderName}
+                  </p>
+                </div>
+
+                <div className="">
+                  <p className="text-white text-2xl font-extralight opacity-60">
+                    Expires
+                  </p>
+                  <p className="text-white text-2xl">{cardExpires}</p>
+                </div>
+
+                <div className="">
+                  <p className="text-white text-2xl font-extralight opacity-60">
+                    CVV
+                  </p>
+                  <p className="text-white text-2xl">{cardCVV}</p>
+                </div>
               </div>
             </div>
+          </div>
 
-            <div></div>
-
-            <div></div>
+          <div className="basis-4/12">
+            <div className="h-full content-end">
+              <p className="text-end text-xl font-light text-white opacity-70">
+                {cardType}
+              </p>
+            </div>
           </div>
         </div>
       </div>
