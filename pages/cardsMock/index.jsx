45b9ddd3cd9f027516c29a79b79145db7601d0ee
Deep Vadukiya <dipvadukiya06@gmail.com>
2024-10-30 13:17:33 +0530

    MG02: Credit Card Mock - added input fields and respective state management with actual functionality

--- pages/cardsMock/index.jsx
+++ pages/cardsMock/index.jsx
@@ -1,6 +1,6 @@
 //
 
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import html2canvas from "html2canvas";
 import { HardDriveDownload, House } from "lucide-react";
@@ -14,6 +14,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
 // utility
 import { APP_PATH } from "../../routes/paths";
 import CardChip from "../../assets/creditCard/cardChip";
@@ -23,19 +25,35 @@ import Nfc from "../../assets/creditCard/Nfc";
 
 // Note: Credit card size: 1011*638 px
 
+const CARD_MOCK_DATA = {
+  bankName: "Your Bank",
+  bankPartnerName: "Your Bank Partner",
+
+  cardCategory: "Rupay",
+  cardSubCategory: "PLATINUM",
+  backgroundColor: "#8CC0DE",
+
+  cardType: "credit_card",
+
+  cardNumber: "1234 1234 1234 1234",
+  cardHolderName: "JOHN DOE",
+  cardExpires: "2036-09",
+  cardCVV: "123",
+
+  isNFCEnabled: true,
+};
+
+const CARD_TYPES = {
+  credit_card: "Credit Card",
+  debit_card: "Debit Card",
+  prepaid_card: "Prepaid Card",
+  charge_card: "Charge Card",
+};
+
 export default function CardMock() {
   const navigate = useNavigate();
 
-  const bankName = "HDFC Bank";
-  const bankPartnerName = "TATA NEUCARD PLUS";
-  const cardCategory = "Rupay";
-  const cardTypeCategory = "PLATINUM";
-  const backgroundColor = "#8CC0DE";
-  const cardType = "CREDIT CARD";
-  const cardNumber = "6529 2500 0082 1348";
-  const cardHolderName = "DEEP VADUKIYA";
-  const cardExpires = "04/28";
-  const cardCVV = "04/28";
+  const [cardDetails, setCardDetails] = useState(CARD_MOCK_DATA);
 
   const handleDownloadImage = async () => {
     const element = document.getElementById("print"),
@@ -49,6 +67,29 @@ export default function CardMock() {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+  };
+
+  const cardDetailUpdate = (e, fieldName) => {
+    if (fieldName === "cardType" || fieldName === "isNFCEnabled") {
+      setCardDetails({
+        ...cardDetails,
+        [fieldName]: e,
+      });
+    } else {
+      setCardDetails({
+        ...cardDetails,
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
+
+  const getFormattedDate = (typedDate) => {
+    const date = new Date(typedDate);
+    const formatted =
+      [date.getMonth() + 1].toString().padStart(2, "0") +
+      "/" +
+      date.getFullYear().toString().substring(2);
+    return formatted;
   };
 
   return (
@@ -69,27 +110,109 @@ export default function CardMock() {
       <div className="grid lg:grid-cols-2">
         <div className="">
           <div className="flex flex-col gap-2 lg:w-3/4">
-            <Input type="text" placeholder="Bank Name" />
-            <Input type="text" placeholder="Partner Name" />
-            <Input type="text" placeholder="Card Category" />
-            <Input type="text" placeholder="Card Sub-Category" />
+            <Input
+              type="text"
+              value={cardDetails.bankName}
+              onChange={cardDetailUpdate}
+              name="bankName"
+              placeholder="Bank Name"
+            />
+            <Input
+              type="text"
+              value={cardDetails.bankPartnerName}
+              onChange={cardDetailUpdate}
+              name="bankPartnerName"
+              placeholder="Partner Name"
+            />
+            <Input
+              type="text"
+              value={cardDetails.cardCategory}
+              onChange={cardDetailUpdate}
+              name="cardCategory"
+              placeholder="Card Category"
+            />
+            <Input
+              type="text"
+              value={cardDetails.cardSubCategory}
+              onChange={cardDetailUpdate}
+              name="cardSubCategory"
+              placeholder="Card Sub-Category"
+            />
 
-            <Input type="text" placeholder="Card Number" />
-            <Input type="text" placeholder="Card Holder" />
-            <Input type="date" placeholder="Card Expiry" />
-            <Input type="text" placeholder="Card CVV" />
+            <Input
+              type="text"
+              value={cardDetails.cardNumber}
+              onChange={cardDetailUpdate}
+              name="cardNumber"
+              placeholder="Card Number"
+            />
+            <Input
+              type="text"
+              value={cardDetails.cardHolderName}
+              onChange={cardDetailUpdate}
+              name="cardHolderName"
+              placeholder="Card Holder"
+            />
+            <Input
+              type="month"
+              value={cardDetails.cardExpires}
+              onChange={cardDetailUpdate}
+              name="cardExpires"
+              placeholder="Card Expiry"
+            />
+            <Input
+              type="text"
+              value={cardDetails.cardCVV}
+              onChange={cardDetailUpdate}
+              name="cardCVV"
+              placeholder="Card CVV"
+            />
 
-            <Select>
+            <Select
+              value={cardDetails.cardType}
+              onValueChange={(val) => cardDetailUpdate(val, "cardType")}
+              name="cardType"
+            >
               <SelectTrigger>
                 <SelectValue placeholder="Card Type" />
               </SelectTrigger>
               <SelectContent>
                 <SelectItem value="credit_card">Credit Card</SelectItem>
                 <SelectItem value="debit_card">Debit Card</SelectItem>
-                <SelectItem value="prepaidt_card">Prepaid Card</SelectItem>
+                <SelectItem value="prepaid_card">Prepaid Card</SelectItem>
                 <SelectItem value="charge_card">Charge Card</SelectItem>
               </SelectContent>
             </Select>
+
+            <div className="flex items-center space-x-2 p-2">
+              <Label
+                htmlFor="terms"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Is NFC Enabled ...?
+              </Label>
+              <Switch
+                name="isNFCEnabled"
+                checked={cardDetails.isNFCEnabled}
+                onCheckedChange={(e) => cardDetailUpdate(e, "isNFCEnabled")}
+              />
+            </div>
+
+            <div className="flex items-center space-x-2 p-2">
+              <Label
+                htmlFor="terms"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Choose Background Color
+              </Label>
+              <input
+                type="color"
+                value={cardDetails.backgroundColor}
+                onChange={cardDetailUpdate}
+                name="backgroundColor"
+                placeholder="Background color"
+              />
+            </div>
           </div>
         </div>
 
@@ -97,32 +220,42 @@ export default function CardMock() {
           <div className="md:p-8 overflow-x-auto" id="print">
             <div
               className="container mx-auto p-5 rounded-3xl shadow-xl flex flex-col justify-between"
-              style={{ backgroundColor, maxWidth: 440, height: 260 }}
+              style={{
+                backgroundColor: cardDetails.backgroundColor ?? "#8CC0DE",
+                maxWidth: 440,
+                height: 260,
+              }}
             >
               <div className="w-full flex justify-between">
                 <div className="">
-                  <p className="text-xl font-semibold text-white">{bankName}</p>
-                  <p className="text-white opacity-70">{bankPartnerName}</p>
+                  <p className="text-xl font-semibold text-white">
+                    {cardDetails.bankName}
+                  </p>
+                  <p className="text-white opacity-70">
+                    {cardDetails.bankPartnerName}
+                  </p>
                 </div>
 
                 <div className="justify-end">
                   <p className="text-xl font-semibold text-white text-end">
-                    {cardCategory}
+                    {cardDetails.cardCategory}
                   </p>
-                  <p className="font-thin text-white">{cardTypeCategory}</p>
+                  <p className="font-thin text-white">
+                    {cardDetails.cardSubCategory}
+                  </p>
                 </div>
               </div>
 
               <div className="w-full flex justify-between">
                 <CardChip height={32} width={32} />
-                <Nfc height={32} width={32} />
+                {cardDetails.isNFCEnabled && <Nfc height={32} width={32} />}
               </div>
 
               <div className="flex flex-row">
                 <div className="basis-8/12">
                   <div className="w-full flex flex-col justify-between">
                     <p className="text-white font-extralight text-2xl tracking-widest pb-2">
-                      {cardNumber}
+                      {cardDetails.cardNumber}
                     </p>
 
                     <div className="flex justify-between">
@@ -130,21 +263,25 @@ export default function CardMock() {
                         <p className="text-white font-extralight opacity-60 max-w-fit">
                           Card Holder
                         </p>
-                        <p className="text-white max-w-fit">{cardHolderName}</p>
+                        <p className="text-white max-w-fit">
+                          {cardDetails.cardHolderName}
+                        </p>
                       </div>
 
                       <div className="">
                         <p className="text-white font-extralight opacity-60">
                           Expires
                         </p>
-                        <p className="text-white">{cardExpires}</p>
+                        <p className="text-white">
+                          {getFormattedDate(cardDetails.cardExpires)}
+                        </p>
                       </div>
 
                       <div className="">
                         <p className="text-white font-extralight opacity-60">
                           CVV
                         </p>
-                        <p className="text-white">{cardCVV}</p>
+                        <p className="text-white">{cardDetails.cardCVV}</p>
                       </div>
                     </div>
                   </div>
@@ -153,7 +290,7 @@ export default function CardMock() {
                 <div className="basis-4/12">
                   <div className="h-full content-end">
                     <p className="text-end font-light text-white opacity-70 text-sm">
-                      {cardType}
+                      {CARD_TYPES[cardDetails.cardType] ?? ""}
                     </p>
                   </div>
                 </div>
