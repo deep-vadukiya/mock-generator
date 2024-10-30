12a86c72bd301a800bfeab5e2b30299dc2a11781
Deep Vadukiya <dipvadukiya06@gmail.com>
2024-10-30 19:34:14 +0530

    MG05: tilwind css configuration and fixed styling issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./@/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
