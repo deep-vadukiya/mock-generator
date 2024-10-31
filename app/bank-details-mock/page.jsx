c02a45d104cd0f7e303cd77b8c4e87f72d3c2d85
Deep Vadukiya <dipvadukiya06@gmail.com>
2024-10-31 11:46:56 +0530

    MG04: bank details: added custom inputs for dynamic details

--- app/bank-details-mock/page.jsx
+++ app/bank-details-mock/page.jsx
@@ -5,14 +5,22 @@
 import React, { useState } from "react";
 import { House, CircleAlert } from "lucide-react";
 import Link from "next/link";
+// icons
+import { User } from "lucide-react";
 // components
 import { buttonVariants } from "@/components/ui/button";
-import { User } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 // ------------------------------------------------
 
 const BANK_MOCK_DATA = {
   backgroundColor: "#FFFFFF",
+  themeColor: "#111111",
+
+  userName: "John Doe",
+  userEmail: "john.doe@gmail.com",
+  bankLogo: "City Bank",
 
   bankProvider: "City Bank",
   accountHolderName: "Mr. John Doe",
@@ -36,6 +44,13 @@ const DETAILS_LIST = [
 export default function Page() {
   const [bankDetails, setBankDetails] = useState(BANK_MOCK_DATA);
 
+  const bankDetailsUpdate = (e) => {
+    setBankDetails({
+      ...bankDetails,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="container mx-auto p-4 space-y-6 sm:pb-8">
       <div className="flex items-center gap-4">
@@ -58,55 +73,197 @@ export default function Page() {
 
       <div className="grid lg:grid-cols-2">
         <div className="">
-          <div className="flex flex-col gap-2 lg:w-3/4">{/* === */}</div>
+          <div className="flex flex-col gap-2 lg:w-3/4">
+            <div className="mb-4 flex flex-col gap-2">
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="userName">User Name</Label>
+                <Input
+                  type="text"
+                  value={bankDetails.userName}
+                  onChange={bankDetailsUpdate}
+                  name="userName"
+                  placeholder="User Name"
+                />
+              </div>
 
-          <div
-            className="-order-1 lg:order-1 overflow-x-auto block"
-            style={{ overflow: "scroll" }}
-          >
-            <div className="md:p-8 p-4 flex justify-center" id="print">
-              <div
-                className="container p-5 rounded-3xl shadow-xl flex flex-col"
-                style={{
-                  backgroundColor: bankDetails.backgroundColor ?? "#FFFFFF",
-                  width: 480,
-                  height: 480,
-                  minWidth: 460,
-                }}
-              >
-                <div className="w-full flex justify-between items-center border-b p-2 pb-8 border-gray-100">
-                  <div className="flex gap-4 justify-center items-center">
-                    <div className="bg-slate-200 rounded-full p-2 h-10 w-10 flex justify-center items-center">
-                      <User />
-                    </div>
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="userEmail">User Email</Label>
+                <Input
+                  type="text"
+                  value={bankDetails.userEmail}
+                  onChange={bankDetailsUpdate}
+                  name="userEmail"
+                  placeholder="User Email"
+                />
+              </div>
 
-                    <div>
-                      <p className="leading-5">Dimitry Vegas</p>
-                      <p className="text-muted-foreground leading-5 text-sm">
-                        useremail001@gmail.com
-                      </p>
-                    </div>
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="bankLogo">Bank Logo Text</Label>
+                <Input
+                  type="text"
+                  value={bankDetails.bankLogo}
+                  onChange={bankDetailsUpdate}
+                  name="bankLogo"
+                  placeholder="Bank Logo Text"
+                />
+              </div>
+            </div>
+
+            <div className="mb-4 flex flex-col gap-2">
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="bankProvider">Bank Provider</Label>
+                <Input
+                  type="text"
+                  value={bankDetails.bankProvider}
+                  onChange={bankDetailsUpdate}
+                  name="bankProvider"
+                  placeholder="Bank Provider"
+                />
+              </div>
+
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="accountHolderName">Account Holder Name</Label>
+                <Input
+                  type="text"
+                  value={bankDetails.accountHolderName}
+                  onChange={bankDetailsUpdate}
+                  name="accountHolderName"
+                  placeholder="Account Holder Name"
+                />
+              </div>
+
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="accountNumber">Account Number</Label>
+                <Input
+                  type="text"
+                  value={bankDetails.accountNumber}
+                  onChange={bankDetailsUpdate}
+                  name="accountHolderNumber"
+                  placeholder="Account Number"
+                />
+              </div>
+
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="ifscCode">IFSC Code</Label>
+                <Input
+                  type="text"
+                  value={bankDetails.ifscCode}
+                  onChange={bankDetailsUpdate}
+                  name="ifscCode"
+                  placeholder="IFSC Code"
+                />
+              </div>
+
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="accountType">Account Type</Label>
+                <Input
+                  type="text"
+                  value={bankDetails.accountType}
+                  onChange={bankDetailsUpdate}
+                  name="accountType"
+                  placeholder="Account Type"
+                />
+              </div>
+
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="branchName">Branch Name</Label>
+                <Input
+                  type="text"
+                  value={bankDetails.branchName}
+                  onChange={bankDetailsUpdate}
+                  name="branchName"
+                  placeholder="Branch Name"
+                />
+              </div>
+
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="transactionFacilities">
+                  Transaction Facilities
+                </Label>
+                <Input
+                  type="text"
+                  value={bankDetails.transactionFacilities}
+                  onChange={bankDetailsUpdate}
+                  name="transactionFacilities"
+                  placeholder="Transaction Facilities"
+                />
+              </div>
+
+              <div className="flex items-center space-x-2 p-2">
+                <Label
+                  htmlFor="terms"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Choose Theme Color
+                </Label>
+                <input
+                  type="color"
+                  value={bankDetails.themeColor}
+                  onChange={bankDetailsUpdate}
+                  name="themeColor"
+                  placeholder="Theme Color"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="-order-1 lg:order-1 overflow-x-auto block"
+          style={{ overflow: "scroll" }}
+        >
+          <div className="md:p-8 p-4 flex justify-center" id="print">
+            <div
+              className="container p-5 rounded-3xl shadow-xl flex flex-col"
+              style={{
+                backgroundColor: bankDetails.backgroundColor ?? "#FFFFFF",
+                width: 480,
+                height: 480,
+                minWidth: 460,
+              }}
+            >
+              <div className="w-full flex justify-between items-center border-b p-2 pb-8 border-gray-100">
+                <div className="flex gap-4 justify-center items-center">
+                  <div
+                    className="bg-slate-200 rounded-full p-2 h-10 w-10 flex justify-center items-center"
+                    style={{ borderColor: bankDetails.themeColor || "#111111" }}
+                  >
+                    <User color={bankDetails.themeColor || "#111111"} />
                   </div>
 
                   <div>
-                    <p className="font-extrabold text-xl">HDFC Bank</p>
+                    <p className="leading-5">{bankDetails.userName}</p>
+                    <p className="text-muted-foreground leading-5 text-sm">
+                      {bankDetails.userEmail}
+                    </p>
                   </div>
                 </div>
 
-                {DETAILS_LIST.map((el) => {
-                  return (
-                    <div
-                      key={el.key}
-                      className="w-full flex justify-between items-center py-3 px-2 border-gray-100 [&:not(:last-child)]:border-b"
-                    >
-                      <p className="text-base">{el.value}</p>
-                      <p className="text-base text-muted-foreground">
-                        {BANK_MOCK_DATA[el.key]}
-                      </p>
-                    </div>
-                  );
-                })}
+                <div>
+                  <p
+                    className="font-extrabold text-xl"
+                    style={{
+                      color: bankDetails.themeColor || "#111111",
+                    }}
+                  >
+                    {bankDetails.bankLogo}
+                  </p>
+                </div>
               </div>
+
+              {DETAILS_LIST.map((el) => {
+                return (
+                  <div
+                    key={el.key}
+                    className="w-full flex justify-between items-center py-3 px-2 border-gray-100 [&:not(:last-child)]:border-b"
+                  >
+                    <p className="text-base">{el.value}</p>
+                    <p className="text-base text-muted-foreground">
+                      {BANK_MOCK_DATA[el.key]}
+                    </p>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
